15a49085995f30e354bbc78ab4e586a811e7c0a3
krishpranav <krisna.pranav@gmail.com>
2021-04-03 12:30:33 +0530

    done changes

--- wifiddos.py
+++ wifiddos.py
@@ -24,3 +24,25 @@ def check_for_essid(essid, lst):
 			check_status = False
 
 	return check_status
+
+print(r"WIFI DDOS")
+
+if not 'SUDO_UID' in os.environ.keys():
+	print("run this tool with sudo")
+	exit()
+
+for file_name in os.listdir():
+	if ".csv" in file_name:
+		print("There shouldn't be any .csv files in your directory")
+		directory = os.getcwd()
+		try:
+			os.mkdir(directory + "/backup")
+		except:
+			print("Back up folder exists.")
+
+		timestap = datetime.now()
+
+		shutil.move(file_name, directory + "/backup/" + str(timestamp) + "-" + file_name)
+
+wlan_pattern = re.compile("^wlan[0-9]+")
+check_wifi_result = wlan_pattern.findall(subprocess.run(["iwconfig"], capture_output=True).stdout.decode())
